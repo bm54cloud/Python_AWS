61743c235b364474ac88a7c9600ef86a055fc246
Brandi McCall <bm54cloud@gmail.com>
2023-02-14 19:20:22 +0000

    feb 14

--- stop_all_instances.py
+++ stop_all_instances.py
@@ -4,19 +4,19 @@ import boto3
 
 region = 'us-east-1' # Insert the region of the instances
 ec2 = boto3.client('ec2', region_name=region)
-instance_attr = ec2.describe_instances()
+instance_attr = ec2.describe_instances() #Lists mulitple dictionaries of instance information
 
-data = instance_attr["Reservations"]
+data = instance_attr["Reservations"] #Call the reservations key
 
-instance_list = []
+instance_list = [] #Create an emppty list
 
 for instances in data:
-    instance = instances["Instances"]
+    instance = instances["Instances"] #Call the Instances key
     for ids in instance:
-        instance_id = ids["InstanceId"]
-        instance_list.append(instance_id)
+        instance_id = ids["InstanceId"] #Iterate through the instances' instance ids
+        instance_list.append(instance_id) #Add the instance_id iterations to the empty list
       
-ec2.stop_instances(InstanceIds=instance_list)
+ec2.stop_instances(InstanceIds=instance_list) # Stop the instances found in instance_list
 
 
     
